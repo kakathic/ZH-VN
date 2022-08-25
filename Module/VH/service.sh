c220b25928f7498c32fea1b7f8b0d5ddfa99c020
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-26 00:01:50 +0700

    Update service.sh

--- Module/VH/service.sh
+++ Module/VH/service.sh
@@ -10,8 +10,6 @@ sleep 2
 fi
 done
 
-sleep 5
-
 pm disable com.miui.analytics
 pm disable com.xiaomi.joyose
 # pm disable com.google.android.gms/.chimera.GmsIntentOperationService
@@ -20,6 +18,20 @@ for Ksksn in $(pm list packages -3 | cut -d : -f2); do
 dumpsys deviceidle whitelist +$Ksksn
 cmd appops set $Ksksn RUN_IN_BACKGROUND allow
 am set-standby-bucket $Ksksn 10
+cmd package compile -m speed $Ksksn
 done
 
-[ -e /data/data/com.miui.personalassistant/files/maml/res/0 ] && Appvault.sh | tee -a ${0%/*}/widget.log
+( [ -e /data/data/com.miui.personalassistant/files/maml/res/0 ] && Appvault.sh | tee -a ${0%/*}/widget.log ) & (
+
+db_dir="/data/data/com.miui.powerkeeper/databases"
+alias user_configure="/data/tools/bin/sqlite3 $db_dir/user_configure.db"
+fps_group='{"list60":"","list90":""}'
+user_configure "update misc set value = '$fps_group' where name = 'fps_group'"
+user_configure "update misc set value = '$fps_group' where name = 'fps_top_video_pkg'"
+user_configure "update misc set value = '$fps_group' where name = 'fps_exclude_pkg'"
+user_configure "update misc set value = '$fps_group' where name = 'fps_smart_group'"
+user_configure "update misc set value = '$fps_group' where name = 'key_priv_names'"
+
+pm disable com.miui.powerkeeper/com.miui.powerkeeper.statemachine.PowerStateMachineService
+#pm enable com.miui.powerkeeper/com.miui.powerkeeper.statemachine.PowerStateMachineService
+)
