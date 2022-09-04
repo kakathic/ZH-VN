aa428c1d10dc5d410fc115af01d354af5dda0ec6
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-04 20:36:12 +0700

    Update install.sh

--- Module/install.sh
+++ Module/install.sh
@@ -52,6 +52,7 @@ abort
 elif [ "$Key" == "KEY_VOLUMEUP" ];then
 [ "$input" == "$input2" ] && input=1 || input=$(($input + 1))
 ui_print2 "$input"
+input swipe 500 500 500 0 20
 sleep 0.3
 Kgg
 elif [ "$Key" == "KEY_VOLUMEDOWN" ];then
