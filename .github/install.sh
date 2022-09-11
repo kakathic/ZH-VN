80bfe404b03a0bc9e566eb7495bf4645118a6f48
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-11 22:00:25 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -10,7 +10,7 @@ done
 
 TTM "Zip
 Up"
-ls
+
 apktool(){ java -jar $TOOLS/apktool-2.6.2.jar "$@"; }     
 baksmali(){ java -jar $TOOLS/baksmali-2.3.4.jar d "$@"; }     
 smali(){ java -jar $TOOLS/smali-2.5.2.jar a "$@"; }     
@@ -26,7 +26,7 @@ echo '{
 "changelog": "https://raw.githubusercontent.com/kakathic/ZH-VN/ZH/Web/Version.md"
 }' > $TOME/Up/$1.json
 cd $TOME/.github/$1
-zip -r "$TOME/Zip/$1-$Vs.Zip" *
+zip -qr "$TOME/Zip/$1-$Vs.Zip" *
 }
 
 Taoup Gapps
@@ -35,4 +35,3 @@ Taoup Pinstaller
 Taoup Theme
 Taoup ZH-VN
 
-ls $TOME/Zip
