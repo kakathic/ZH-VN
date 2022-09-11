85400deb7d7fa27dc97aabc40130d49e2321e6d6
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-11 20:25:59 +0700

    Delete Module directory

--- Module/VH/uninstall.sh
+++
@@ -1,5 +0,0 @@
-rm -fr /data/system/theme_magic/*
-rm -fr /data/system/theme/*
-rm -fr /data/system/users/*/wallpaper*
-rm -fr /data/system/package_cache/*
-rm -fr /data/resource-cache/*
--- Module/uninstall.sh
+++
@@ -1,2 +0,0 @@
-[ -e ${0%/*}/Global.txt ] && rm -fr /data/miui/* $(find /data/app -name *com.xiaomi.discover*)
-rm -fr /data/dalvik-cache/*/*framework* /data/system/package_cache/* /data/resource-cache/* /data/tools/backup
