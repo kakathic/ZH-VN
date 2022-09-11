4df3b5b5b513ebd2386cfaa2030665c7558b339b
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-11 21:37:49 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -10,7 +10,7 @@ done
 
 TTM "Zip
 Up"
-
+ 
 apktool(){ java -jar $TOOLS/apktool-2.6.2.jar "$@"; }     
 baksmali(){ java -jar $TOOLS/baksmali-2.3.4.jar d "$@"; }     
 smali(){ java -jar $TOOLS/smali-2.5.2.jar a "$@"; }     
