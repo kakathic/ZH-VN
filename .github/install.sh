261aaa2dec645076610ce31bffad1cfbc4a45d0a
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-11 21:11:50 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -3,6 +3,14 @@
 TOME="$GITHUB_WORKSPACE"
 TOOLS="$TOME/.github/Tools"
 
+TTM(){
+for vah in $1; do
+[ -e $vah ] || mkdir -p "$TOME/$vah"
+done
+}
+
+TTM "Zip"
+
 apktool(){ java -jar $TOOLS/apktool-2.6.2.jar "$@"; }     
 baksmali(){ java -jar $TOOLS/baksmali-2.3.4.jar d "$@"; }     
 smali(){ java -jar $TOOLS/smali-2.5.2.jar a "$@"; }     
@@ -22,4 +30,5 @@ echo '{
 "changelog": "https://raw.githubusercontent.com/kakathic/ZH-VN/ZH/Web/Version.md"
 }' > $GITHUB_WORKSPACE/Up/Up.json
 
-7za a -tzip -o $TOME/Zip/Gapps.Zip $TOME/.github/Gapps/*
+7za a -r -tzip -o $TOME/Zip/Gapps.Zip $TOME/.github/Gapps/*
+ls $TOME
