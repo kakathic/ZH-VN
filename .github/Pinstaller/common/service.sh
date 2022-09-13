cb53a0772ad5edc842011a4af1a0a20674114bf0
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-13 12:57:23 +0700

    Delete .github/Pinstaller directory

--- .github/Pinstaller/common/service.sh
+++
@@ -1,7 +0,0 @@
-# Kakathic
-
-# Home module magisk
-MODPATH="${0%/*}"
-. $MODPATH/Tools.sh
-
-# Later in the booting process, the class late_start will be triggered, and Magisk “service” mode will be started. In this mode, service scripts are executed.
