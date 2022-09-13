89722bfa4bcd36fac6eb270bf2559274230a9ebe
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-13 23:52:57 +0700

    Update post-fs-data.sh

--- .github/ZH-VN/common/post-fs-data.sh
+++ .github/ZH-VN/common/post-fs-data.sh
@@ -4,6 +4,4 @@ MODPATH="${0%/*}"
 . $MODPATH/Tools.sh
 
 # This triggers on post-fs-data when /data is decrypted and mounted. The daemon magiskd will be launched, post-fs-data scripts are executed, and module files are magic mounted
-
-rm -fr /data/system/package_cache/*
-rm -fr /data/resource-cache/*
+rm -fr /data/system/package_cache/* /data/resource-cache/*
