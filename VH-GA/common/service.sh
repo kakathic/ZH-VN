e9e183bcedb91bd3e7af2099571a2873b84d497a
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-19 16:08:38 +0700

    Update service.sh

--- VH-GA/common/service.sh
+++ VH-GA/common/service.sh
@@ -5,6 +5,14 @@ MODPATH="${0%/*}"
 . $MODPATH/Tools.sh
 
 # Later in the booting process, the class late_start will be triggered, and Magisk “service” mode will be started. In this mode, service scripts are executed.
+while [ "$(getprop sys.boot_completed)" != 1 ]; do
+Auto=$(($Auto + 1))
+if [ "$Auto" == 100 ] && [ "$API" -ge 30 ];then
+echo > $MODPATH/disable
+reboot
+fi
+sleep 1
+done
 
 # Code
 module_exec() {
