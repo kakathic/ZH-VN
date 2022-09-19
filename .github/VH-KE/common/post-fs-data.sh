a74585998bc97f509960ddad592fc4afc292e411
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-19 14:23:12 +0700

    Delete .github/VH-KE directory

--- .github/VH-KE/common/post-fs-data.sh
+++
@@ -1,7 +0,0 @@
-# Kakathic
-# Home module magisk
-MODPATH="${0%/*}"
-. $MODPATH/Tools.sh
-# This triggers on post-fs-data when /data is decrypted and mounted. The daemon magiskd will be launched, post-fs-data scripts are executed, and module files are magic mounted
-[ "$API" -ge 31 ] && rm -fr /data/dalvik-cache/*/*framework*
-rm -fr /data/system/package_cache/* /data/resource-cache/*
