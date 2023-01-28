b16b9d6989f3e6da3fc1b6f1f6e91b91c6f76c40
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-01-28 14:26:05 +0700

    Update install.sh

--- VH-PT/install.sh
+++ VH-PT/install.sh
@@ -200,7 +200,6 @@ if [ "$apkcai" == 1 ];then
 VB="iget p0, p0, Landroid\/content\/pm\/ApplicationInfo;->uid:I"
 Thaythe "$VB" "$VB \n const/4 v1, 0x1 \n return v1" "$(Timkiem "$VB" "com.miui.packageinstaller/classes*")"
 
-
 Vsmali ".method public static final c(Landroid\/content\/pm\/ApplicationInfo;)Z" \
 ".end method" \
 '.method public static final c(Landroid\/content\/pm\/ApplicationInfo;)Z
@@ -266,6 +265,33 @@ Vsmali ".method private static isAdValid" \
     return p0
 .end method' \
 "$TMPDIR/Apk/com.android.thememanager/classes*"
+
+Vsmali ".method public isAuthorizedResource()Z" \
+".end method" \
+'.method public isAuthorizedResource()Z
+    .registers 2
+     const/4 v0, 0x1
+    return v0
+.end method' \
+"$TMPDIR/Apk/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
+
+Vsmali ".method public isCanNotPlay()Z" \
+".end method" \
+'.method public isCanNotPlay()Z
+    .registers 4
+    const/4 v0, 0x1
+    return v0
+.end method' \
+"$TMPDIR/Apk/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
+
+Vsmali ".method public isProductBought()Z" \
+".end method" \
+'.method public isProductBought()Z
+    .registers 2
+    const/4 v0, 0x1
+    return v0
+.end method' \
+"$TMPDIR/Apk/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
 
 Thaythe "iget-boolean v1, v0, Lcom\/android\/thememanager\/detail\/theme\/model\/OnlineResourceDetail;->bought:Z" "iget-boolean v1, v0, Lcom\/android\/thememanager\/detail\/theme\/model\/OnlineResourceDetail;->bought:Z \n const/4 v0, 0x1 \n return v0" "$TMPDIR/Apk/com.android.thememanager/classes*"
 
