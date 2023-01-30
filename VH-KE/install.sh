a91cdadd723ebc9e89e8c04cf94fe8582e1995c3
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-01-30 15:44:47 +0700

    Update install.sh

--- VH-KE/install.sh
+++ VH-KE/install.sh
@@ -244,6 +244,7 @@ Vsmali '.method public static isNotSupported()Z' \
 .end method' \
 "$TMPDIR/Apk/com.android.settings/classes*/com/android/settings/lab/*"
 
+Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x1" ''$TMPDIR/Apk/$miuik'framework/classes*/miui/securityspace/XSpaceConstant.smali'
 Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x1" "$TMPDIR/Apk/com.android.systemui/classes*/com/android/systemui/navigationbar/*.smali"
 Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x1" "$TMPDIR/Apk/com.android.systemui/classes*/com/android/systemui/qs"
 Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x1" "$TMPDIR/Apk/com.android.systemui/classes*/com/android/settingslib/inputmethod/*.smali"
