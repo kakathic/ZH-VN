0644f8ae7bc5136aaeefffc5a14363aa2e1940b8
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-01-31 01:56:03 +0700

    Update install.sh

--- FL-GA/install.sh
+++ FL-GA/install.sh
@@ -7,120 +7,31 @@ ui_print " "
 [ "$Tkkg" == 1 ] && unzip -qo "$ZIP" 'lib/*' -d /tmp || abort
 chmod -R 755 $TMPI
 
-
 ui_print "  Cai dat"
 ui_print " "
 
 [ "$API" == 32 ] && API=31
 [ -e "$TMPI/FL-$API.zip" ] && unzip -qo "$TMPI/FL-$API.zip" -d $TMPI || abort "- Loi khong tim thay Gapps, API: $API"
-set_perm_recursive $TMPI 0 0 0755 0644 2>/dev/null
-
-[ -e /system/product/overlay ] && Tsssj="/system"
-[ -e /system/system_ext/priv-app ] && Tsskk="/system"
-
-if [ "$API" -le 29 ];then
-Lxxx="$Tsssj/product/app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
-$Tsssj/product/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
-$Tsssj/product/app/GoogleLocationHistory/GoogleLocationHistory.apk
-$Tsssj/product/app/LatinImeGoogle/LatinImeGoogle.apk
-$Tsssj/product/app/ModuleMetadataGooglePrebuilt/ModuleMetadataGooglePrebuilt.apk
-$Tsssj/product/priv-app/AndroidAutoStub/AndroidAutoStub.apk
-$Tsssj/product/priv-app/CarrierConfig/CarrierConfig.apk
-$Tsssj/product/priv-app/CarrierServices/CarrierServices.apk
-$Tsssj/product/priv-app/ConfigUpdater/ConfigUpdater.apk
-$Tsssj/product/priv-app/GmsCore/GmsCore.apk
-$Tsssj/product/priv-app/GoogleFeedback/GoogleFeedback.apk
-$Tsssj/product/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
-$Tsssj/product/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
-$Tsssj/product/priv-app/GooglePlayServicesUpdater/GooglePlayServicesUpdater.apk
-$Tsssj/product/priv-app/GoogleRestore/GoogleRestore.apk
-$Tsssj/product/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
-$Tsssj/product/priv-app/SetupWizard/SetupWizard.apk"
-fi
-
-if [ "$API" == 30 ];then
-Lxxx="$Tsssj/product/app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
-$Tsssj/product/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
-$Tsssj/product/app/GoogleLocationHistory/GoogleLocationHistory.apk
-$Tsssj/product/app/LatinImeGoogle/LatinImeGoogle.apk
-$Tsssj/product/app/com.google.android.modulemetadata/com.google.android.modulemetadata.apk
-$Tsssj/product/priv-app/AndroidAutoStub/AndroidAutoStub.apk
-$Tsssj/product/priv-app/CarrierServices/CarrierServices.apk
-$Tsssj/product/priv-app/ConfigUpdater/ConfigUpdater.apk
-$Tsssj/product/priv-app/GmsCore/GmsCore.apk
-$Tsssj/product/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
-$Tsssj/product/priv-app/GooglePlayServicesUpdater/GooglePlayServicesUpdater.apk
-$Tsssj/product/priv-app/GoogleRestore/GoogleRestore.apk
-$Tsskk/system_ext/priv-app/GoogleFeedback/GoogleFeedback.apk
-$Tsskk/system_ext/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
-$Tsskk/system_ext/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
-$Tsskk/system_ext/priv-app/SetupWizard/SetupWizard.apk"
-fi
-
-if [ "$API" == 31 ] ||  "$API" == 32 ];then
-Lxxx="$Tsssj/product/app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
-$Tsssj/product/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
-$Tsssj/product/app/GoogleLocationHistory/GoogleLocationHistory.apk
-$Tsssj/product/app/LatinImeGoogle/LatinImeGoogle.apk
-$Tsssj/product/app/com.google.android.modulemetadata/com.google.android.modulemetadata.apk
-$Tsssj/product/app/com.google.mainline.telemetry/com.google.mainline.telemetry.apk
-$Tsssj/product/priv-app/AndroidAutoStub/AndroidAutoStub.apk
-$Tsssj/product/priv-app/CarrierServices/CarrierServices.apk
-$Tsssj/product/priv-app/GmsCore/GmsCore.apk
-$Tsssj/product/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
-$Tsssj/product/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
-$Tsssj/product/priv-app/GooglePlayServicesUpdater/GooglePlayServicesUpdater.apk
-$Tsssj/product/priv-app/GoogleRestore/GoogleRestore.apk
-$Tsskk/system_ext/priv-app/GoogleFeedback/GoogleFeedback.apk
-$Tsskk/system_ext/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
-$Tsskk/system_ext/priv-app/SetupWizard/SetupWizard.apk"
-fi
-
-if [ "$API" -ge 33 ];then
-Lxxx="$Tsssj/product/app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
-$Tsssj/product/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
-$Tsssj/product/app/GoogleLocationHistory/GoogleLocationHistory.apk
-$Tsssj/product/app/LatinImeGoogle/LatinImeGoogle.apk
-$Tsssj/product/app/com.google.android.modulemetadata/com.google.android.modulemetadata.apk
-$Tsssj/product/app/com.google.mainline.telemetry/com.google.mainline.telemetry.apk
-$Tsssj/product/priv-app/AndroidAutoStub/AndroidAutoStub.apk
-$Tsssj/product/priv-app/CarrierServices/CarrierServices.apk
-$Tsssj/product/priv-app/ConfigUpdater/ConfigUpdater.apk
-$Tsssj/product/priv-app/GmsCore/GmsCore.apk
-$Tsssj/product/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
-$Tsssj/product/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
-$Tsssj/product/priv-app/GooglePlayServicesUpdater/GooglePlayServicesUpdater.apk
-$Tsssj/product/priv-app/GoogleRestore/GoogleRestore.apk
-$Tsssj/product/priv-app/PersonalSafety/PersonalSafety.apk
-$Tsskk/system_ext/priv-app/GoogleFeedback/GoogleFeedback.apk
-$Tsskk/system_ext/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk"
-fi
-
-for gwmeg in $Lxxx; do
-[ -e "$gwmeg" ] && rm -fr ${gwmeg%/*}
-[ -L "$gwmeg" ] && rm -fr ${gwmeg%/*}
-done
+chmod 755 $TMPI 2>/dev/null
 
 if [ -e $TMPI/product ];then
-[ -e /system/product/overlay ] && PRODUCT="/system/product" || PRODUCT="/product"
-cp -rf $TMPI/product/* $PRODUCT
+cp -rf $TMPI/product/* ${Overlay%/*}
 fi
 
 if [ -e $TMPI/system_ext ];then
-[ -e /system/system_ext/app ] && SYSTEM_EXT="/system/system_ext" || SYSTEM_EXT="/system_ext"
-cp -rf $TMPI/system_ext/* $SYSTEM_EXT
+cp -rf $TMPI/system_ext/* $(find /system_ext /*/system_ext /*/*/system_ext -type f 2>/dev/null | head -n1)
 fi
 
 # Dịch vụ gg
-Ptkkf="$(echo $Tsssj/*/etc/permissions/*cn.google*.xml)"
+Ptkkf="$(find /*/etc/permissions/*cn.google*.xml /*/*/etc/permissions/*cn.google*.xml /*/*/*/etc/permissions/*cn.google*.xml -type f 2>/dev/null | head -n1)"
 if [ "$(grep -cm1 cn.google.services $Ptkkf)" == 1 ];then
 sed -i -e '/cn.google.services/d' -e '/services_updater/d' "$Ptkkf"
 fi
 
-fnfnte="/system/priv-app/MiBrowser/MiBrowser.apk
+fnfnte="${Buildprop%/*}/priv-app/MiBrowser/MiBrowser.apk
 /cust/priv-app/MiBrowser/MiBrowser.apk
 /cust/app/SogouInput/SogouInput.apk
-/system/app/SogouInput/SogouInput.apk
+${Buildprop%/*}/app/SogouInput/SogouInput.apk
 "
 for kkddx in $fnfnte; do
 [ -e "$kkddx" ] && rm -fr ${kkddx%/*}
