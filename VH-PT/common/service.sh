42217a040891771255d8cc701c89094be0ebe491
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 22:35:04 +0700

    Update service.sh

--- VH-PT/common/service.sh
+++ VH-PT/common/service.sh
@@ -1,6 +1,6 @@
 # Kakathic
-
 # Automatically turn off the module if 100 seconds wait at the logo
+
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
 Auto=$(($Auto + 1))
 if [ "$Auto" == 100 ];then
@@ -25,8 +25,9 @@ fi
 done
 
 for ksik in $(ls -1 ${0%/*}/app); do
-Pathapk="$(pm path "$ksik" | cut -d : -f2)"
+tenapk="$(echo -n "$ksik" | base64 -d)"
+Pathapk="$(pm path "$tenapk" | cut -d : -f2)"
 chcon u:object_r:apk_data_file:s0 ${0%/*}/app/$ksik
 su -mm -c mount -o bind ${0%/*}/app/$ksik "$Pathapk"
-( cmd package compile -m speed $ksik ) &
+( cmd package compile -m speed $tenapk ) &
 done
