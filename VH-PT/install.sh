8822a34da83b802e8bdeb3ab6b8c6df5a489b972
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 09:10:38 +0700

    Add files via upload

--- VH-PT/install.sh
+++ VH-PT/install.sh
@@ -8,51 +8,18 @@ PROPFILE=true
 POSTFSDATA=true
 ## Set to true for service.sh to be used
 LATESTARTSERVICE=true
-## After installing the module, it will open the magisk application if you are in another application 
-OPENMG=true
-
-## Language text
-chon="Chọn"
-time="Tổng thời gian"
-h="giờ"
-m="phút"
-s="giây"
-Information="Thông tin"
-author="Tác giả"
-name="Tên"
-unzip="Đang xử lý..."
-volkey1="! Sử dụng phím âm lượng"
-volkey2="! Vol- = Chọn số hiện tại, Vol+ = Chuyển đổi số."
-volkey3="! Ấn nút nguồn để hủy."
-load="Tải"
-error="! Lỗi không tìm thấy hoặc lỗi mạng !"
-error2="- Mô-đun này chỉ chạy trên thiết bị arm64, của bạn là $ARCH !"
 
 # Internet
 User="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
 Viewonline(){
 [ -e /system/bin/curl ] && curl -s -k -G -L -H "$User" --connect-timeout 20 "$1" || wget -q --header "$User" --no-check-certificate -O - "$1"; }
-Viewonline "https://raw.githubusercontent.com/kakathic/ZH-VN/ZH/.github/Tools/Tools.sh" > $MODPATH/Tools.sh
-. $MODPATH/Tools.sh; [ "$TTvip" == 1 ] || abort "$error";
+Viewonline "https://raw.githubusercontent.com/kakathic/ZH-VN/ZH/.github/Tools/Main.sh" > $TMPDIR/Main.sh
+[ " $(grep -c '# Kakathic' $TMPDIR/Main.sh)" == 1 ] && . $TMPDIR/Main.sh || abort "$error";
 
-## Check the system devices
-[ "$ARCH" == "arm64" ] || abort "$error2"
-
-## Introduce
-print_modname(){
-ui_print
-ui_print "  $name: $(GP name) $(GP version)"
-ui_print
-ui_print "  $author: $(GP author)"
-ui_print
-ui_print "  $Information: $(getprop ro.product.device), $API, $ARCH"
-ui_print
-}
 
 ## Start the installation
 on_install(){
 
-echo 'I0FwaWx0PSQoZ3JlcCAtbTEgY29tLnhpYW9taSAvZGF0YS9zeXN0ZW0vc3luYy8qLiogfCB0ciAnICcgJ1xuJyB8IGdyZXAgLW0xIGFjY291bnQ9IHwgY3V0IC1kIFwiIC1mMikKI1sgLXogIiRBcGlsdCIgXSAmJiBBcGlsdD0kKGdyZXAgLWFDNyAnY29tLmFuZHJvaWQuY29udGFjdHMnIC9kYXRhL3N5c3RlbS9zeW5jL2FjY291bnRzLnhtbCB8IGdyZXAgLyB8IGhlYWQgLW4xIHwgY3V0IC1kICcvJyAtZjEpCgpbICIkQVBJIiAtbGUgMzAgXSAmJiBBcGlsdD0iJChncmVwIC1tMSAiY29tLnhpYW9taSIgL2RhdGEvc3lzdGVtL3N5bmMvYWNjb3VudHMueG1sIHwgdHIgJyAnICdcbicgfCBncmVwIC1tMSBhY2NvdW50IHwgY3V0IC1kIFwiIC1mMikiClsgIiRBUEkiIC1nZSAzMSBdICYmIEFwaWx0PSIkKGdyZXAgLWEgLUEyICJjb20ueGlhb21pIiAvZGF0YS9zeXN0ZW0vc3luYy9hY2NvdW50cy54bWwgfCBoZWFkIC1uMiB8IHRhaWwgLW4xIHwgY3V0IC1kICcvJyAtZjEpIgpbIC16ICIkQXBpbHQiIF0gJiYgQXBpbHQ9Ikzhu5dpIFRLIE1pIgoKaW1laT0iJChnZXRwcm9wIHBlcnNpc3QucmFkaW8ubWVpZCkiClsgLXogIiRpbWVpIiBdICYmIGltZWk9IiQoZ2V0cHJvcCByby5yaWwub2VtLm1laWQpIgpbIC16ICIkaW1laSIgXSAmJiBpbWVpPSIkKGdyZXAgLW0xIGtleV9tZWlkX3Nsb3QwIC9kYXRhLyovMC9jb20uYW5kcm9pZC5waG9uZS9zaGFyZWRfcHJlZnMvY29tLmFuZHJvaWQucGhvbmVfcHJlZmVyZW5jZXMueG1sIHwgY3V0IC1kICc+JyAtZjIgfCBjdXQgLWQgJzwnIC1mMSkiClsgLXogIiRpbWVpIiBdICYmIGltZWk9Ikzhu5dpIE1FSUQpIgoKTGl0dXNzPSIkKFhlbSBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20va2FrYXRoaWMvWkgtVk4vWkgvVXNlcm5hbWUvJChnZXRwcm9wIHJvLnByb2R1Y3QuZGV2aWNlKSB8IGJhc2U2NCAtZCkiCgpmb3IgVmtkZyBpbiAkTGl0dXNzOyBkbwpra2loaD0kKGdyZXAgLWFjbTEgJFZrZGcgL2RhdGEvc3lzdGVtL3N5bmMvYWNjb3VudHMueG1sKQpbICRra2loaCA9PSAxIF0gJiYgYnJlYWsKZG9uZQoKZGtucmpqPSIkKGVjaG8gIiRMaXR1c3MiIHwgZ3JlcCAtY20xICRpbWVpKSIKClRlc3QxMjM9dHJ1ZQoKaWYgWyAiJGtraWhoIiA9PSAxIF0gfHwgWyAiJGRrbnJqaiIgPT0gMSBdO3RoZW4KdWlfcHJpbnQyICJDaMOgbyBi4bqhbjogJEFwaWx0Igp1aV9wcmludAoKWyAtZSAvZGF0YS90b29scy9saWIvTG9nLnR4dCBdICYmIHJtIC1mciAvZGF0YS90b29scy9saWIvTG9nLnR4dAplbHNlCnVpX3ByaW50ICIhIFRow7RuZyBiw6FvCiIKdWlfcHJpbnQgIiAgVMOqbiBtw6F5OiAkKGdldHByb3Agcm8ucHJvZHVjdC5kZXZpY2UpCgogIFTDoGkga2hv4bqjbiBNaTogJEFwaWx0CiAgCiAgSU1FSTogJGltZWkKIiAgCnVpX3ByaW50ICIgIMSQ4buDIHPhu60gZOG7pW5nIGzDonUgZMOgaSBi4bqhbiBuw6puIOG7p25nIGjhu5kgxJHhu4Mgc+G7rSBk4bulbmcKCiAgdsSpbmggdmnhu4VuLgoKICBUaOG7rSBuZ2hp4buHbSBz4bq9IGPDsyB0w6FjIGThu6VuZyB0cm9uZyBuZ8OgeSBow7RtIMSRw7MuCgogIEjhur90IHRo4budaSBnaWFuIHPhur0gdOG7sSDEkeG7mW5nIHJlYm9vdCBtw6F5LgoiClsgLWUgL2RhdGEvdG9vbHMvbGliL0xvZy50eHQgXSAmJiBhYm9ydCAiLSBC4bqhbiDEkcOjIGjhur90IGzGsOG7o3QgZMO5bmcgdGjhu60gcsO5aS4KIgphbSBzdGFydCAtYSBhbmRyb2lkLmludGVudC5hY3Rpb24uVklFVyAtZCAiaHR0cHM6Ly9rYWthdGhpYy5naXRodWIuaW8vWkgtVk4vU3VwcG9ydC5odG1sIiA+JjIKCmVjaG8gJwp3aGlsZSB0cnVlOyBkbwppZiBbICIkKGNhdCAvZGF0YS90b29scy9saWIvVGVzdCkiICE9ICIkKGRhdGUgKyIlZCIpIiBdO3RoZW4KZWNobyAiSOG6v3QgVGltZTogJChkYXRlKSIgPiAvZGF0YS90b29scy9saWIvTG9nLnR4dAplY2hvID4gJHswJS8qfS9yZW1vdmUKc2xlZXAgMTAKcmVib290CmZpCmRvbmUKJyA+IC9kYXRhL3Rvb2xzL2xpYi9ydW4uc2gKZWNobyAnCm5vaHVwIC9kYXRhL3Rvb2xzL2xpYi9ydW4uc2ggJj4kezAlLyp9L3J1bi5sb2cgJgonID4+ICRUTVBESVIvc2VydmljZS5zaAplY2hvICIkKGRhdGUgKyIlZCIpIiA+ICRNT0RQQVRIL1Rlc3QKZmk=' | base64 -d > $TMPDIR/ka.sh && . $TMPDIR/ka.sh
 ui_print "! Sử dụng phím âm lượng"
 ui_print "! Vol- = Chọn số hiện tại, Vol+ = Chuyển đổi số."
 ui_print "! Ấn nút nguồn để hủy."
@@ -164,7 +131,7 @@ Xu_install toybox
 sed(){ toybox sed "$@";}
 cut(){ toybox cut "$@";}
 
-TTM "$TMPDIR/Apk/tmp
+TTM "$APK/tmp
 /data/tools/apk"
 
 echo "ro.product.vip=$(getprop ro.product.device)_global" >> $TMPDIR/system.prop
@@ -201,26 +168,26 @@ fi
 fi
 
 if [ "$apkcai" == 1 ];then
-VB="iget p0, p0, Landroid\/content\/pm\/ApplicationInfo;->uid:I"
+VB="iget p0, p0, Landroid/content/pm/ApplicationInfo;->uid:I"
 Thaythe "$VB" "$VB \n const/4 v1, 0x1 \n return v1" "$(Timkiem "$VB" "com.miui.packageinstaller/classes*")"
 
-Vsmali ".method public static final c(Landroid\/content\/pm\/ApplicationInfo;)Z" \
+Vsmali ".method public static final c(Landroid/content/pm/ApplicationInfo;)Z" \
 ".end method" \
-'.method public static final c(Landroid\/content\/pm\/ApplicationInfo;)Z
+'.method public static final c(Landroid/content/pm/ApplicationInfo;)Z
     .registers 3
     const/4 v1, 0x1
     return v1
 .end method' \
-"$TMPDIR/Apk/com.miui.packageinstaller/classes*"
+"$APK/com.miui.packageinstaller/classes*"
 
-Vsmali ".method public static q(Landroid\/content\/pm\/ApplicationInfo;)Z" \
+Vsmali ".method public static q(Landroid/content/pm/ApplicationInfo;)Z" \
 ".end method" \
-'.method public static q(Landroid\/content\/pm\/ApplicationInfo;)Z
+'.method public static q(Landroid/content/pm/ApplicationInfo;)Z
     .registers 3
     const/4 v1, 0x1
     return v1
 .end method' \
-"$TMPDIR/Apk/com.miui.packageinstaller/classes*"
+"$APK/com.miui.packageinstaller/classes*"
 fi
 
 if [ "$tietkiem" == 1 ];then
@@ -237,7 +204,7 @@ com/miui/powerkeeper/ui
 com/miui/powerkeeper/utils"
 
 for vhjs in $Bebrb; do
-Thaythe "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "Lmiui/os/Build;->IS_MIUI:Z" "$TMPDIR/Apk/com.miui.powerkeeper/classes*/$vhjs/*.smali"
+Thaythe "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "Lmiui/os/Build;->IS_MIUI:Z" "$APK/com.miui.powerkeeper/classes*/$vhjs/*.smali"
 done
 
 Vsmali ".method public static isFeatureOn()Z" \
@@ -247,7 +214,7 @@ Vsmali ".method public static isFeatureOn()Z" \
     const/4 v1, 0x0
     return v1
 .end method' \
-"$TMPDIR/Apk/com.miui.powerkeeper/classes*/*"
+"$APK/com.miui.powerkeeper/classes*/*"
 
 fi
 
@@ -259,7 +226,7 @@ Vsmali ".method public isVideoAd()Z" \
     const/4 v0, 0x0
     return v0
 .end method' \
-"$TMPDIR/Apk/com.android.thememanager/classes*"
+"$APK/com.android.thememanager/classes*"
 
 Vsmali ".method private static isAdValid" \
 ".end method" \
@@ -268,7 +235,7 @@ Vsmali ".method private static isAdValid" \
     const/4 p0, 0x0
     return p0
 .end method' \
-"$TMPDIR/Apk/com.android.thememanager/classes*"
+"$APK/com.android.thememanager/classes*"
 
 Vsmali ".method public isAuthorizedResource()Z" \
 ".end method" \
@@ -277,7 +244,7 @@ Vsmali ".method public isAuthorizedResource()Z" \
      const/4 v0, 0x1
     return v0
 .end method' \
-"$TMPDIR/Apk/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
+"$APK/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
 
 Vsmali ".method public isCanNotPlay()Z" \
 ".end method" \
@@ -286,7 +253,7 @@ Vsmali ".method public isCanNotPlay()Z" \
     const/4 v0, 0x1
     return v0
 .end method' \
-"$TMPDIR/Apk/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
+"$APK/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
 
 Vsmali ".method public isProductBought()Z" \
 ".end method" \
@@ -295,34 +262,34 @@ Vsmali ".method public isProductBought()Z" \
     const/4 v0, 0x1
     return v0
 .end method' \
-"$TMPDIR/Apk/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
+"$APK/com.android.thememanager/classes*/com/android/thememanager/basemodule/resource/model/*"
 
-Thaythe "iget-boolean v1, v0, Lcom\/android\/thememanager\/detail\/theme\/model\/OnlineResourceDetail;->bought:Z" "iget-boolean v1, v0, Lcom\/android\/thememanager\/detail\/theme\/model\/OnlineResourceDetail;->bought:Z \n const/4 v0, 0x1 \n return v0" "$TMPDIR/Apk/com.android.thememanager/classes*"
+Thaythe "iget-boolean v1, v0, Lcom/android/thememanager/detail/theme/model/OnlineResourceDetail;->bought:Z" "iget-boolean v1, v0, Lcom/android/thememanager/detail/theme/model/OnlineResourceDetail;->bought:Z \n const/4 v0, 0x1 \n return v0" "$APK/com.android.thememanager/classes*"
 
-Thaythe "ro.miui.region" "ro.khu.vuc.cn" "$TMPDIR/Apk/com.android.thememanager/classes*"
-Thaythe "ro.product.mod_device" "ro.product.modcn" "$TMPDIR/Apk/com.android.thememanager/classes*"
-Thaythe "DRM_ERROR_UNKNOWN" "DRM_SUCCESS" "$TMPDIR/Apk/com.android.thememanager/classes*"
-Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x0" "$TMPDIR/Apk/com.android.thememanager/classes*/*"
+Thaythe "ro.miui.region" "ro.khu.vuc.cn" "$APK/com.android.thememanager/classes*"
+Thaythe "ro.product.mod_device" "ro.product.modcn" "$APK/com.android.thememanager/classes*"
+Thaythe "DRM_ERROR_UNKNOWN" "DRM_SUCCESS" "$APK/com.android.thememanager/classes*"
+Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x0" "$APK/com.android.thememanager/classes*/*"
 
-if [ "$(grep -cm1 'DRM_SUCCESS' $TMPDIR/Apk/com.miui.system/classes*/miui/drm/DrmManager.smali)" == 1 ];then
-Vsmali ".method private static isPermanentRights(Lmiui\/drm\/DrmManager\$RightObject;)Z" \
+if [ "$(grep -cm1 'DRM_SUCCESS' $APK/com.miui.system/classes*/miui/drm/DrmManager.smali)" == 1 ];then
+Vsmali ".method private static isPermanentRights(Lmiui/drm/DrmManager\$RightObject;)Z" \
 ".end method" \
 '.method private static isPermanentRights(Lmiui/drm/DrmManager$RightObject;)Z
     .registers 5
     const/4 v0, 0x1
     return v0
 .end method' \
-"$TMPDIR/Apk/com.miui.system/classes*/miui/drm/DrmManager.smali"
+"$APK/com.miui.system/classes*/miui/drm/DrmManager.smali"
 
-Thaythe "DRM_ERROR_UNKNOWN" "DRM_SUCCESS" "$TMPDIR/Apk/com.miui.system/classes*/miui/drm/DrmManager.smali"
+Thaythe "DRM_ERROR_UNKNOWN" "DRM_SUCCESS" "$APK/com.miui.system/classes*/miui/drm/DrmManager.smali"
 else
-rm -fr $TMPDIR/Apk/com.miui.system*
+rm -fr $APK/com.miui.system*
 fi
 
 fi
 
 if [ "$baomat" == 1 ];then
-Autoone "Lmiui/os/Build;->IS_DEVELOPMENT_VERSION:Z" "0x1" "$TMPDIR/Apk/com.miui.securitycenter/classes*/com/miui/dock/*.smali"
+Autoone "Lmiui/os/Build;->IS_DEVELOPMENT_VERSION:Z" "0x1" "$APK/com.miui.securitycenter/classes*/com/miui/dock/*.smali"
 
 Listbm="com/miui/securityscan
 com/miui/securityscan/b0
@@ -338,10 +305,10 @@ com/miui/securityscan/ui/main
 com/miui/securityscan/ui/settings"
 
 for vkks in $Listbm; do
-Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x1" "$TMPDIR/Apk/com.miui.securitycenter/classes*/$vkks/*.smali"
+Autoone "Lmiui/os/Build;->IS_INTERNATIONAL_BUILD:Z" "0x1" "$APK/com.miui.securitycenter/classes*/$vkks/*.smali"
 done
 
-Autoone "Lmiui/os/Build;->IS_STABLE_VERSION:Z" "0x1" "$TMPDIR/Apk/com.miui.securitycenter/classes*/com/miui/permcenter/x/*.smali"
+Autoone "Lmiui/os/Build;->IS_STABLE_VERSION:Z" "0x1" "$APK/com.miui.securitycenter/classes*/com/miui/permcenter/x/*.smali"
 fi
 
 ui_print
