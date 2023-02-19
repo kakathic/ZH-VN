52e7359a1669459eed232a1b1b434214f1d5ad0f
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 13:59:35 +0700

    Update service.sh

--- VH-PT/common/service.sh
+++ VH-PT/common/service.sh
@@ -1,13 +1,9 @@
 # Kakathic
 
-# Home module magisk
-MODPATH="${0%/*}"
-. $MODPATH/Tools.sh
-
 # Automatically turn off the module if 100 seconds wait at the logo
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
 Auto=$(($Auto + 1))
-if [ "$Auto" == 100 ] && [ "$API" -ge 30 ];then
+if [ "$Auto" == 100 ];then
 echo > $MODPATH/disable
 reboot
 fi
