c2ef2c7d529894f3fe8c3e89664a2abf6c5857a6
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 22:10:40 +0700

    Update service.sh

--- VH-KE/common/service.sh
+++ VH-KE/common/service.sh
@@ -1,5 +1,4 @@
 # Kakathic
-
 # Later in the booting process, the class late_start will be triggered, and Magisk “service” mode will be started. In this mode, service scripts are executed.
 
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
