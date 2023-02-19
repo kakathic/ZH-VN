f30f68ea523a28db6fa86fa7c55fce58270cb6eb
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 14:00:10 +0700

    Update service.sh

--- VH-KE/common/service.sh
+++ VH-KE/common/service.sh
@@ -1,14 +1,10 @@
 # Kakathic
 
-# Home module magisk
-MODPATH="${0%/*}"
-. $MODPATH/Tools.sh
-
 # Later in the booting process, the class late_start will be triggered, and Magisk “service” mode will be started. In this mode, service scripts are executed.
 
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
 Auto=$(($Auto + 1))
-if [ "$Auto" == 100 ] && [ "$API" -ge 30 ];then
+if [ "$Auto" == 100 ];then
 echo > $MODPATH/disable
 reboot
 fi
