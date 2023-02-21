be978e2d531052f21fd104dc5a13ed295952e64c
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-22 04:34:00 +0700

    Update uninstall.sh

--- VH-GA/uninstall.sh
+++ VH-GA/uninstall.sh
@@ -1,4 +1,3 @@
 # kakathic
 [ -e ${0%/*}/NO ] && rm -fr $(find /data/app -name *com.android.vending*) $(find /data/app -name *com.google.android.gms*)
-rm -fr /data/system/package_cache/* /data/resource-cache/*
-
+rm -fr /data/system/package_cache/* /data/resource-cache/* /data/dalvik-cache/*/*framework*
