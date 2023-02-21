0a8d8c9031c5d04f99e9b79c4236bb1c7f82a2b9
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-22 04:35:03 +0700

    Update uninstall.sh

--- VH-KE/uninstall.sh
+++ VH-KE/uninstall.sh
@@ -1,3 +1,3 @@
 # kakathic
-[ -e ${0%/*}/Global.txt ] && rm -fr /data/miui/* $(find /data/app -name *com.xiaomi.discover*)
+
 rm -fr /data/system/package_cache/* /data/resource-cache/* /data/dalvik-cache/*/*framework*
