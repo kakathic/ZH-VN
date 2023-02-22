4b88de62dffb71c87d65a133c9bc5480314cd4fe
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-22 22:15:42 +0700

    Update post-fs-data.sh

--- VH-PT/common/post-fs-data.sh
+++ VH-PT/common/post-fs-data.sh
@@ -1,4 +1,5 @@
 # Kakathic
-
 # This triggers on post-fs-data when /data is decrypted and mounted. The daemon magiskd will be launched, post-fs-data scripts are executed, and module files are magic mounted
-rm -fr /data/system/package_cache/* /data/resource-cache/* /data/dalvik-cache/*/*framework*
+rm -fr /data/dalvik-cache/*
+rm -fr /data/system/package_cache/*
+rm -fr /data/resource-cache/*
