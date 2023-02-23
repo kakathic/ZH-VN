8c2b1afd840fd9545bdfd0e076799be247af3861
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-23 13:54:02 +0700

    Update service.sh

--- VH-GA/common/service.sh
+++ VH-GA/common/service.sh
@@ -100,7 +100,6 @@ fi
 
 
 if [ ! -e $MODPATH/ON ];then
-sleep 500
 am start com.google.android.apps.restore/com.google.android.apps.pixelmigrate.migrate.component.D2dWizardManager
 echo > $MODPATH/ON
 fi
