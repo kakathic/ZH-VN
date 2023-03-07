6d5805d9a3fbfef224d37434e52427a8da6123e0
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-07 23:21:16 +0700

    Update install.sh

--- VH-ZH/install.sh
+++ VH-ZH/install.sh
@@ -164,7 +164,7 @@ cp -rf MiLanProVF.ttf RobotoVF.ttf
 if [ "$fontvh" == 1 ];then
 ui_print2 "Cài phông chữ"
 ui_print
-[ -e "$VHMI/fonts/MiLanProVF.ttf" ] && cp -rf $VHMI/fonts $MODPATH/system
+[ -e "$VHMI/fonts/MiLanProVF.ttf" ] && cp -rf $VHMI/fonts $MODPATH/system | cp -rf /system/fonts/Roboto-Regular.ttf $MODPATH/system/fonts/MiLanProVF.ttf
 lnf
 else
 ui_print2 "Xoá phông chữ"
