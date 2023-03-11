55be2145f809c40e5b7b7aab2217925f919fed57
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-12 06:58:48 +0700

    Add files via upload
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

--- VH-ZH/common/post-fs-data.sh
+++ VH-ZH/common/post-fs-data.sh
@@ -1,5 +1,9 @@
 # Kakathic
 # This triggers on post-fs-data when /data is decrypted and mounted. The daemon magiskd will be launched, post-fs-data scripts are executed, and module files are magic mounted
 #[ "$(getprop ro.product.cpu.abi)" -ge 31 ] && rm -fr /data/dalvik-cache/*
-rm -fr /data/system/package_cache/*
-rm -fr /data/resource-cache/*
+#rm -fr /data/system/package_cache/*
+#rm -fr /data/resource-cache/*
+resetprop ro.khu.vuc VN
+resetprop ro.vi.tri vn
+resetprop ro.khu.vuc.cn CN
+resetprop ro.vi.tri.cn cn
--- VH-ZH/common/service.sh
+++ VH-ZH/common/service.sh
@@ -1,17 +1,11 @@
 #!/system/bin/sh
 
-resetprop ro.khu.vuc VN
-resetprop ro.vi.tri vn
-resetprop ro.khu.vuc.cn CN
-resetprop ro.vi.tri.cn cn
-
 # Code
 # Automatically turn off the module if 100 seconds wait at the logo
-MODPATH="${0%/*}"
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
 Auto=$(($Auto + 1))
 if [ "$Auto" == 100 ];then
-echo > $MODPATH/disable
+echo > ${0%/*}/disable
 rm -fr /data/dalvik-cache/*
 rm -fr /data/system/package_cache/*
 rm -fr /data/resource-cache/*
@@ -24,19 +18,19 @@ done
 # Continues operation if the module is not disabled 
 # Code
 
-RD="$RANDOM"
 while true; do
-echo > /sdcard/$RD
-if [ -e /sdcard/$RD ];then
-rm -fr /sdcard/$RD
+if [ -e /sdcard/Android ];then
 break
 else
-sleep 2
+sleep 1
 fi
 done
 
 cmd settings put global GPUTUNER_SWITCH true
 pm disable com.miui.analytics
+
+# Hỗ trợ nếu bị treo
+/system/bin/Support &>/dev/null &
 
 sleep 200
 for Ksksn in $(pm list packages -3 | cut -d : -f2); do
