715eaab8c94ddf18d8004d49681ec717b2de0c04
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-12 16:36:37 +0700

    Update service.sh
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

--- VH-KE/common/service.sh
+++ VH-KE/common/service.sh
@@ -2,14 +2,6 @@
 # Later in the booting process, the class late_start will be triggered, and Magisk “service” mode will be started. In this mode, service scripts are executed.
 # Automatically turn off the module if 100 seconds wait at the logo
 
-for fwten in $(find ${0%/*}/framework/*.txt); do
-su -mm -c mount -o bind ${fwten%.*} "$(cat $fwten)"
-done
-
-for tenapk in $(find ${0%/*}/sys_app/*.txt); do
-su -mm -c mount -o bind ${tenapk%.*} "$(cat $tenapk)"
-done
-
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
 Auto=$(($Auto + 1))
 if [ "$Auto" == 100 ];then
