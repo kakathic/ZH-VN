e86ba172f7a8adfa7e38a5fcf6d1a40a17429054
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-13 21:23:26 +0700

    Update Main.sh
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

--- .github/Tools/Main.sh
+++ .github/Tools/Main.sh
@@ -91,7 +91,7 @@ ui_print2 "MOD: $RANDOM -> $RANDOM";
 Xan "MOD THAYTHE: $1 -> $2";
 for Ttcfg in $(Timkiem "$1" "$3"); do
 [ -e "$Ttcfg" ] && sed -i "s|${1//\//\\\/}|${2//\//\\\/}|g" $Ttcfg || Xan "- Lỗi: $1";
-[ -e "$Ttcfg" ] && echo "$Ttcfg" >> "$APK/$(echo "$3" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+[ -e "$Ttcfg" ] && echo "$Ttcfg" 2>/dev/null >> "$APK/$(echo "$3" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 [ -e "$Ttcfg" ] && Xan "$APK/$(echo "$3" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
@@ -102,7 +102,7 @@ for skd in $(Timkiem "$1" "$4"); do
 [ -e $skd ] && ui_print2 "MOD: $RANDOM";
 [ -e $skd ] && Xan "MOD CHOSE: $1" || Xan "- Lỗi: $(echo "$1" | sed 's|\\||g')";
 [ -e $skd ] && sed -i -e "/${1//\//\\\/}/,/${2//\//\\\/}/c const/4 $ndbdhd, $3" "$skd";
-[ -e $skd ] && echo "$skd" >> "$APK/$(echo "$4" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+[ -e $skd ] && echo "$skd" 2>/dev/null >> "$APK/$(echo "$4" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 [ -e $skd ] && Xan "$APK/$(echo "$4" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
@@ -112,7 +112,7 @@ ui_print2 "MOD: $RANDOM -> $RANDOM";
 Xan "MOD AUTOONE: $1 -> $2";
 for vakkddhh in $(find $3 -name "*.smali" -exec grep -l "..., $1" {} + ); do
 eval "sed -i $(grep "..., $1" "$vakkddhh" | awk '{print "-e \"s|sget-boolean "$2" '$1'|const/4 "$2" '$2'|g\"" }' | sort | uniq | tr '\n' ' ') $(echo "$vakkddhh" | sed 's|\$|\\\$|g')";
-echo "$vakkddhh" >> "$APK/$(echo "$3" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+echo "$vakkddhh" 2>/dev/null >> "$APK/$(echo "$3" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 Xan "$APK/$(echo "$3" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
@@ -122,7 +122,7 @@ for Vka in $(Timkiem "$1" "$4"); do
 [ -e $Vka ] && ui_print2 "MOD: $RANDOM";
 [ -e $Vka ] && Xan "MOD VSMALI: $1" || Xan "- Lỗi: $(echo "$1" | sed 's|\\||g')";
 [ -e $Vka ] && sed -i -e "/^${1//\//\\\/}/,/${2//\//\\\/}/c $(echo "$3" | sed -z 's|\n|\\n|g')" "$Vka";
-[ -e $Vka ] && echo "$Vka" >> "$APK/$(echo "$4" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+[ -e $Vka ] && echo "$Vka" 2>/dev/null >> "$APK/$(echo "$4" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 [ -e $Vka ] && Xan "$APK/$(echo "$4" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
