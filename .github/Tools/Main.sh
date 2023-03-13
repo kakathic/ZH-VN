40d332dfa6083d46ad8dcb05e0332a76d25d4a74
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-13 13:23:55 +0700

    Update Main.sh
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

--- .github/Tools/Main.sh
+++ .github/Tools/Main.sh
@@ -82,32 +82,32 @@ smali () { java -Xms150m -Xmx1028m -jar "/data/tools/lib/Tools/smali.jar" "$@"; 
 Timkiem(){ find $2 -name "*.smali" -exec grep -l "${1//\//\\\/}" {} +; }
 
 Thaythe(){
-ui_print2 "MOD: $RANDOM -> $RANDOM"
-Xan "MOD THAYTHE: $1 -> $2"
+ui_print2 "MOD: $RANDOM -> $RANDOM";
+Xan "MOD THAYTHE: $1 -> $2";
 for Ttcfg in $(Timkiem "$1" "$3"); do
-[ -e "$Ttcfg" ] && sed -i "s|${1//\//\\\/}|${2//\//\\\/}|g" $Ttcfg || Xan "- Lỗi: $1"
-[ -e "$Ttcfg" ] && echo "$Ttcfg" >> "$APK/$(echo "$Ttcfg" | sed "s|$APK/||g" | cut -d '/' -f1)/class"
-[ -e "$Ttcfg" ] && echo "$APK/$(echo "$Ttcfg" | sed "s|$APK/||g" | cut -d '/' -f1)/class"
+[ -e "$Ttcfg" ] && sed -i "s|${1//\//\\\/}|${2//\//\\\/}|g" $Ttcfg || Xan "- Lỗi: $1";
+[ -e "$Ttcfg" ] && echo "$Ttcfg" >> "$APK/$(echo "$Ttcfg" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+[ -e "$Ttcfg" ] && echo "$APK/$(echo "$Ttcfg" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
 
 Autoone(){
-ui_print2 "MOD: $RANDOM -> $RANDOM"
-Xan "MOD AUTOONE: $1 -> $2"
+ui_print2 "MOD: $RANDOM -> $RANDOM";
+Xan "MOD AUTOONE: $1 -> $2";
 for vakkddhh in $(find $3 -name "*.smali" -exec grep -l "..., $1" {} + ); do
-eval "sed -i $(grep "..., $1" "$vakkddhh" | awk '{print "-e \"s|sget-boolean "$2" '$1'|const/4 "$2" '$2'|g\"" }' | sort | uniq | tr '\n' ' ') $(echo "$vakkddhh" | sed 's|\$|\\\$|g')"
-echo "$vakkddhh" >> "$APK/$(echo "$vakkddhh" | sed "s|$APK/||g" | cut -d '/' -f1)/class"
-echo "$APK/$(echo "$vakkddhh" | sed "s|$APK/||g" | cut -d '/' -f1)/class"
+eval "sed -i $(grep "..., $1" "$vakkddhh" | awk '{print "-e \"s|sget-boolean "$2" '$1'|const/4 "$2" '$2'|g\"" }' | sort | uniq | tr '\n' ' ') $(echo "$vakkddhh" | sed 's|\$|\\\$|g')";
+echo "$vakkddhh" >> "$APK/$(echo "$vakkddhh" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+echo "$APK/$(echo "$vakkddhh" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
 
 Vsmali(){
 for Vka in $(Timkiem "$1" "$4"); do
-[ -e $Vka ] && ui_print2 "MOD: $RANDOM"
-[ -e $Vka ] && Xan "MOD VSMALI: $1" || Xan "- Lỗi: $(echo "$1" | sed 's|\\||g')"
-[ -e $Vka ] && sed -i -e "/^${1//\//\\\/}/,/${2//\//\\\/}/c $(echo "$3" | sed -z 's|\n|\\n|g')" "$Vka"
-[ -e $Vka ] && echo "$Vka" >> "$APK/$(echo "$Vka" | sed "s|$APK/||g" | cut -d '/' -f1)/class"
-[ -e $Vka ] && echo "$APK/$(echo "$Vka" | sed "s|$APK/||g" | cut -d '/' -f1)/class"
+[ -e $Vka ] && ui_print2 "MOD: $RANDOM";
+[ -e $Vka ] && Xan "MOD VSMALI: $1" || Xan "- Lỗi: $(echo "$1" | sed 's|\\||g')";
+[ -e $Vka ] && sed -i -e "/^${1//\//\\\/}/,/${2//\//\\\/}/c $(echo "$3" | sed -z 's|\n|\\n|g')" "$Vka";
+[ -e $Vka ] && echo "$Vka" >> "$APK/$(echo "$Vka" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
+[ -e $Vka ] && echo "$APK/$(echo "$Vka" | sed "s|$APK/||g" | cut -d '/' -f1)/class";
 done
 }
 
