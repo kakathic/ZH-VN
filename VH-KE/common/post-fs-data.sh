dd70411672987f0cb840fb01e8bb3553c906b200
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-15 13:35:46 +0700

    Update post-fs-data.sh
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

--- VH-KE/common/post-fs-data.sh
+++ VH-KE/common/post-fs-data.sh
@@ -1,10 +1,24 @@
 # Kakathic
 # This triggers on post-fs-data when /data is decrypted and mounted. The daemon magiskd will be launched, post-fs-data scripts are executed, and module files are magic mounted
 
+if [ -e /system/framework/arm64/boot-ext.art ] || [ -e /system/framework/arm/boot-ext.oat ];then
+mount -o rw,remount / || mount -o rw,remount /
+rm -fr /system/framework/arm64/*
+rm -fr /system/framework/arm/*
+mount -o ro,remount / || mount -o ro,remount /
+fi
+
+if [ -e /system/framework/arm64/boot-ext.art ] || [ -e /system/framework/arm/boot-ext.oat ];then
+mkdir -p ${0%/*}/system/framework/arm64
+mkdir -p ${0%/*}/system/framework/arm
+echo > ${0%/*}/system/framework/arm64/.replace
+echo > ${0%/*}/system/framework/arm/.replace
+fi
+
 if [ ! -e ${0%/*}/Check ];then
 rm -fr /data/dalvik-cache/*
-rm -fr /data/system/package_cache/*
-rm -fr /data/resource-cache/*
+#rm -fr /data/system/package_cache/*
+#rm -fr /data/resource-cache/*
 echo > ${0%/*}/Check
 fi
 
