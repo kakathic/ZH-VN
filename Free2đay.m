2f71184079273cdd8eca208a64ecb16fcefc55cf
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-24 22:09:26 +0700

    Create Free2đay.m
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

---
+++ Free2đay.m
@@ -0,0 +1,9 @@
+## Nhập imei 
+
++ Hãy điền số imei của bạn vào bên dưới
+
++ Ví dụ kk=9273747838291872
+
+imei=
+
+Thời gian dùng thử là 2 ngày imei của bạn sẽ được ghi vào hệ thống và sau đó sẽ không được dùng thử nữa.
