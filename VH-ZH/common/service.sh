50a9133a8a173b0653e2d6df550d1a8b495e33c2
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-04-04 20:22:02 +0700

    Update service.sh

--- VH-ZH/common/service.sh
+++ VH-ZH/common/service.sh
@@ -32,9 +32,23 @@ sleep 100
 cmd settings put global GPUTUNER_SWITCH true
 pm disable com.miui.analytics
 
-sleep 100
 for Ksksn in $(pm list packages -3 | cut -d : -f2); do
 dumpsys deviceidle whitelist +$Ksksn >&2
 appops set $Ksksn RUN_ANY_IN_BACKGROUND allow
 done
 
+if [ -e /sdcard/vip ];then
+while true; do
+kncfgvv="$(grep -m1 "ListApp=" ${0%/*}/module.prop | cut -d "=" -f2 | tr ',' '\n')";
+[ "$kncfgvv" ] || kncfgvv="$(pm list packages -3 | cut -d : -f2)";
+for Ksksn in $kncfgvv; do
+dumpsys deviceidle whitelist +$Ksksn >&2
+appops set $Ksksn 10008 allow
+appops set $Ksksn 10021 allow
+appops set $Ksksn RUN_IN_BACKGROUND allow
+appops set $Ksksn RUN_ANY_IN_BACKGROUND allow
+appops set $Ksksn START_FOREGROUND allow
+done
+sleep 120
+done
+fi
